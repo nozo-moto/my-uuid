ceb77e2538909a2954aca0e5830f21a664400ee5
nozo-moto <nozomotoitech@gmail.com>
2021-05-01 20:01:52 +0900

    impl uuidv1

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"io"
+	mathRand "math/rand"
+	"net"
+	"time"
 )
 
 type UUID [16]byte // 128bit
 
 func main() {
+	uuidv1, err := NewV1()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(uuidv1)
+
 	uuidV4, err := NewV4()
 	if err != nil {
 		panic(err)
@@ -18,11 +28,37 @@ func main() {
 }
 
 func NewV1() (uuid UUID, err error) {
-	rander := rand.Reader
-	_, err = io.ReadFull(rander, uuid[:])
+	var gregorianBeginTime time.Time
+	gregorianBeginTime, err = time.Parse(time.RFC3339, "1582-10-15T00:00:00Z")
 	if err != nil {
 		return
 	}
+
+	timestamp := time.Since(gregorianBeginTime).Nanoseconds() / 100
+	// set time_low
+	binary.BigEndian.PutUint32(uuid[0:], uint32(timestamp&0xffffffff))
+	// set time_mid
+	binary.BigEndian.PutUint16(uuid[4:], uint16((timestamp>>32)&0xffff))
+	// set time_hi_and_version
+	binary.BigEndian.PutUint16(uuid[6:], uint16((timestamp>>48)&0xffff))
+
+	// set clk_seq_hi_res set clk_seq_low
+	rander := rand.Reader
+	var b [2]byte
+	if _, err = io.ReadFull(rander, b[:]); err != nil {
+		return
+	}
+	seq := int(b[0])<<8 | int(b[1])
+	clockSeq := uint16(seq&0x3fff) | 0x8000
+	binary.BigEndian.PutUint16(uuid[8:], clockSeq)
+
+	// set macadd for 81~127
+	macAddres, err := getMacAddr()
+	if err != nil {
+		return
+	}
+	node := randomChoiceByteArray(macAddres)[:]
+	copy(uuid[10:], node[:])
 	return
 }
 
@@ -63,4 +99,24 @@ func (uuid UUID) String() string {
 	var dst [36]byte
 	uuid.encodeHex(dst[:])
 	return string(dst[:])
+}
+
+func getMacAddr() ([][]byte, error) {
+	ifas, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	var as [][]byte
+	for _, ifa := range ifas {
+		haddr := ifa.HardwareAddr
+		if len(haddr) > 0 {
+			as = append(as, haddr)
+		}
+	}
+	return as, nil
+}
+
+func randomChoiceByteArray(arr [][]byte) []byte {
+	mathRand.Seed(time.Now().Unix())
+	return arr[mathRand.Intn(len(arr))]
 }
